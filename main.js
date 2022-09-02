385a775ac6f075d33a7eaf5475546478066cc40d
Salim Shrestha <salimshrestha98@gmail.com>
2022-09-02 16:04:04 +0545

    Use Components to get JSX
    Here, we refactored our code and built reusable react components

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-const navbar = (
-    <>
-        <h1>My Site Title</h1>
-        <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </>
-)
+/**
+ * Functions that return JSX are called Components.
+ * Function name must be in PascalCase.
+ * We can call the function name directly or,
+ * Call as components <ComponentName />.
+ * @returns
+ */
+
+function GetContent() {
+    return (
+        <>
+            <h1>My Site Title</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </>
+    )
+}
 
 
-ReactDOM.render( navbar, document.getElementById('root'));
+// ReactDOM.render( GetContent(), document.getElementById('root'));
+ReactDOM.render( <GetContent/>, document.getElementById('root'));
