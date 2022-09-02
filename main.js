f02c5d320d905f435126c216dcdcd31b214a0a5c
Salim Shrestha <salimshrestha98@gmail.com>
2022-09-02 16:39:18 +0545

    Challenge - Create separate components Header and Footer

--- main.js
+++ main.js
@@ -10,22 +10,34 @@
  *
  */
 
-function Page() {
+function Header() {
     return (
-        <>
         <header>
             <nav>
                 <h1>My Site Title</h1>
             </nav>
         </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            &copy; Salim Shrestha. All rights reserved.
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <>
+        <Header />
         <h1>Reasons I love React:</h1>
         <ol>
             <li>I Want to build Single Page Apps.</li>
             <li>I want to get better at Frontend Development.</li>
         </ol>
-        <footer>
-            &copy; Salim Shrestha. All rights reserved.
-        </footer>
+        <Footer />
         </>
     )
 }
