6dedc29a782879bc58a980d64c7c278adfe758bd
Salim Shrestha <salimshrestha98@gmail.com>
2022-09-02 15:55:24 +0545

    Tweak - Use Fragments instead of a parent element
    Here, we replace parent element with what looks like an empty tag that is called as fragments

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const navbar = (
-    <nav>
+    <>
         <h1>My Site Title</h1>
         <ul>
             <li>Pricing</li>
             <li>About</li>
             <li>Contact</li>
         </ul>
-    </nav>
+    </>
 )
 
 
