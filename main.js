823bf93e2ac857bb6479ff9184db8fa899f1f166
Salim Shrestha <salimshrestha98@gmail.com>
2022-09-02 16:08:55 +0545

    Challenge - Crete an ordered list

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
 /**
- * Functions that return JSX are called Components.
- * Function name must be in PascalCase.
- * We can call the function name directly or,
- * Call as components <ComponentName />.
- * @returns
+ * Challenge:
+ * Create a custom Page component that returns an ordered list
+ * of reasons why you are excited to learn react.
  */
 
-function GetContent() {
+function Page() {
     return (
         <>
-            <h1>My Site Title</h1>
-            <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+            <h1>Reasons I love React:</h1>
+            <ol>
+                <li>I Want to build Single Page Apps.</li>
+                <li>I want to get better at Frontend Development.</li>
+            </ol>
         </>
     )
 }
-
-
-// ReactDOM.render( GetContent(), document.getElementById('root'));
-ReactDOM.render( <GetContent/>, document.getElementById('root'));
+const root = document.getElementById('root');
+ReactDOM.render( <Page />, root );
