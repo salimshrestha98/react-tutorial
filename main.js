1ffa13d575966be7dff62d4a4c95b57994a71dd3
Salim Shrestha <salimshrestha98@gmail.com>
2022-09-02 15:40:15 +0545

    Exercise - Create a navbar using html elements

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-ReactDOM.render( <h1>Hello World</h1>, document.getElementById('root'));
+const navbar = (
+    <nav>
+        <h1>My Site Title</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+
+ReactDOM.render( navbar, document.getElementById('root'));
