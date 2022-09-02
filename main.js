f0b013de2440d7a100d9e917f70b6c943b7a6d0c
Salim Shrestha <salimshrestha98@gmail.com>
2022-09-02 16:42:16 +0545

    create component for main content

--- main.js
+++ main.js
@@ -28,15 +28,23 @@ function Footer() {
     )
 }
 
-function Page() {
+function MainContent() {
     return (
         <>
-        <Header />
         <h1>Reasons I love React:</h1>
         <ol>
             <li>I Want to build Single Page Apps.</li>
             <li>I want to get better at Frontend Development.</li>
         </ol>
+        </>
+    )
+}
+
+function Page() {
+    return (
+        <>
+        <Header />
+        <MainContent />
         <Footer />
         </>
     )
